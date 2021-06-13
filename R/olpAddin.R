210c3767f457f16018e7147a15e065d9f27956a8
Kenta Tanaka <tanaka@mana.bi>
2021-06-14 01:38:32 +0900

    change to use browseURL().

--- R/olpAddin.R
+++ R/olpAddin.R
@@ -1,22 +1,23 @@
 olpAddin <- function(){
-    library(shiny)
+#    library(shiny)
     youtube_url <- rstudioapi::showPrompt(title = "講師から指示されたYouTube LiveのURLを入力してください", message = "講師から指示されたYouTube LiveのURLを入力してください")
     bitly_url <- rstudioapi::showPrompt(title = "講師から指示されたLiveCodeのURLを入力してください", message = "講師から指示されたLiveCodeのURLを入力してください")
     tubeplayR::tubeplay(youtube_url)
+    browseURL(bitly_url)
 
-    ui <- fluidPage(titlePanel("Live Code"),
+#    ui <- fluidPage(titlePanel("Live Code"),
+#
+#                    mainPanel(fluidRow(
+#                        htmlOutput("my_test")
+#                    )
+#                    )
+#    )
 
-                    mainPanel(fluidRow(
-                        htmlOutput("my_test")
-                    )
-                    )
-    )
+#    server <- function(input, output) {
+#        output$my_test <- renderUI({
+#            tags$iframe(src=bitly_url, height=1280, width=800)
+#        })
+#    }
 
-    server <- function(input, output) {
-        output$my_test <- renderUI({
-            tags$iframe(src=bitly_url, height=1280, width=800)
-        })
-    }
-
-    shinyApp(ui, server)
+#    shinyApp(ui, server)
 }
