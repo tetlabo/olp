08445c328f9bf9cc1d3a3db5718b72f68557cf00
Kenta Tanaka <tanaka@mana.bi>
2021-06-22 14:24:59 +0900

    ドキュメント (.Rd) のアップデート、プログラムの微修正

--- R/olpAddin.R
+++ R/olpAddin.R
@@ -1,7 +1,7 @@
 olpAddin <- function(){
 #    library(shiny)
     youtube_url <- rstudioapi::showPrompt(title = "講師から指示されたYouTube LiveのURLを入力してください", message = "講師から指示されたYouTube LiveのURLを入力してください")
-    bitly_url <- rstudioapi::showPrompt(title = "講師から指示されたLiveCodeのURLを入力してください", message = "講師から指示されたLiveCodeのURLを入力してください")
+    livecode_url <- rstudioapi::showPrompt(title = "講師から指示されたLiveCodeのURLを入力してください", message = "講師から指示されたLiveCodeのURLを入力してください")
     tubeplayR::tubeplay(youtube_url)
-    browseURL(bitly_url)
+    browseURL(livecode_url)
 }
